1073ada05a96a6e69ecacadd9082b3319da0e75d
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2025-03-18 00:17:09 +0700

    Небольшие изменения

--- Client.Avalonia/Converters/NumericValidationRule.cs
+++ Client.Avalonia/Converters/NumericValidationRule.cs
@@ -13,27 +13,22 @@ public class NumericValidationRule : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is double d)
-        {
-            // Преобразуем в строку с запятой вместо точки
             return d.ToString("G", CultureInfo.GetCultureInfo("ru-RU"));
-        }
+
         return value?.ToString();
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string input)
-        {
-            input = input.Replace('.', ','); // Автоматически заменяем точку на запятую
+        if (value is not string input)
+            return new BindingNotification(new("Введите корректное число!"), BindingErrorType.DataValidationError);
 
-            // Проверяем, является ли введённый текст числом
-            if (NumericRegex.IsMatch(input) && double.TryParse(input, NumberStyles.Any, CultureInfo.GetCultureInfo("ru-RU"), out var result))
-            {
-                return result;
-            }
-        }
+        input = input.Replace('.', ',');
 
-        // Если ввод некорректен — возвращаем ошибку
-        return new BindingNotification(new InvalidOperationException("Введите корректное число!"), BindingErrorType.DataValidationError);
+        if (NumericRegex.IsMatch(input)
+            && double.TryParse(input, NumberStyles.Any, CultureInfo.GetCultureInfo("ru-RU"), out var result))
+            return result;
+
+        return new BindingNotification(new("Введите корректное число"), BindingErrorType.DataValidationError);
     }
 }
